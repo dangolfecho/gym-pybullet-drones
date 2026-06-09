58b5b8c04a38e550c71ccca1140cfec670c86dfd
Gideon <55153800+dangolfecho@users.noreply.github.com>
2026-06-09 16:19:26 +0530

    Creating new schedules

--- gym_pybullet_drones/examples/schedule_creator.py
+++ gym_pybullet_drones/examples/schedule_creator.py
@@ -29,12 +29,12 @@ DEFAULT_C = False
 def create(default=DEFAULT_C):
     if(default):
         start_pos.extend([0, 0, 0])
-        end_pos.extend([0.1, 0.1, 0.1])
+        end_pos.extend([0, 0, 0])
         pos_steplength.extend([0.1, 0.1, 0.1])
-        start_rpys.extend([0, 0, 0])
+        start_rpys.extend([-0.1, -0.1, -0.1])
         end_rpys.extend([0.1, 0.1, 0.1])
         rpy_steplength.extend([0.1, 0.1, 0.1])
-        training_time = 1e5
+        training_time = 1e6
         use_prev_model = True
     else:
         for i in range(3):
